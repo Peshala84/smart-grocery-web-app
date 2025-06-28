92be1a307b4eb5d002d03a8617b7a7697e4c2574
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-06-28 13:06:00 +0530

    update title and descriiption also include the image

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -14,8 +14,12 @@ const BottomBanner = () => {
                     <h1 className='text-2xl md:text-3xl font-semibold text-primary 
                     mb-6'>Why We Are the Best?</h1>
                     {features.map((feature, index)=>(
-                        <div>
-                            
+                        <div key={index} className='flex items-center gap-4 mt-2'>
+                            <img src={feature.icon} alt={feature.title} className='md:w-11 w-9' />
+                            <div>
+                              <h3 className='text-lg md:text-xl font-semibold'>{feature.title}</h3>
+                            <p className='text-gray-500/70 text-xs md:text-sm '>{feature.description}</p>  
+                            </div>
                         </div>
                     ))}
                 </div>

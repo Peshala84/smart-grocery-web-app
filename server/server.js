c1f5e5586aeecfbfbf225664f1a8a549b2cbc458
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-08-01 10:55:22 +0530

    add additional routers for seller, product, cart, address, and order APIs

--- server/server.js
+++ server/server.js
@@ -4,17 +4,24 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import 'dotenv/config.js';
 import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './config/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import addressRouter from './routes/addressRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 await connectDB()
+await connectCloudinary()
 
 //alow multiple origins
 
 const allowedOrigins = [
-  'http://localhost:5173/'
+  'http://localhost:5173'
 ]
 
 //middleware config
@@ -27,6 +34,11 @@ app.get('/', (req, res) =>
 );
 
 app.use('/api/user', userRouter);
+app.use('/api/seller', sellerRouter);
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/address', addressRouter);
+app.use('/api/order', orderRouter);
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
